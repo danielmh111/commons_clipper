7a7633d35361e3912e16ccf52d7b4ea72e1a3b1b
danielmhi111 <danielmartinhill@gmail.com>
2025-11-23 19:44:49 +0000

    calculate start and end segments of clip

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import requests
 from rich.pretty import pprint
 import re
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import datetime, time
+import math
+
 
 BASE_URL = "https://2f0f8fc-az-westeurope-fsly.cdn.redbee.live/ukparliament/parliamentlive"
 
@@ -42,7 +44,6 @@ def fetch_manifest(asset_id: str, material_id: str, resolution: str) -> str:
 
 # gideon speaks at 14:53:14
 
-
 def main():
     asset_id = "03d096f3-4a94-4352-b351-70ad3bcb39cc_0D62A9b"
     material_id = "IAz5cD8Tg7_0D62A9b"
@@ -51,19 +52,30 @@ def main():
     manifest = fetch_manifest(asset_id=asset_id, material_id=material_id, resolution=resolution)
     lines: list[str] = [line for line in manifest.split("\n")]
 
-    pprint(lines)
-
     duration_target = int([line.split(":") for line in lines if re.match(".*TARGETDURATION.*",line)][0][1])
-    print(duration_target)
-
-    actual_durations = [line for line in lines if re.match("#EXINF:")]
-    print(actual_durations)
-
+    actual_durations = [float(p) for line in lines for part in line.split(",") for p in part.split(":") if re.match("#EXTINF:", part) and not re.match("#EXTINF", p)]
+    unique_durations = len(set(actual_durations))
     start_time = datetime.fromisoformat([line.split(":")  for line in lines if re.match(".*PROGRAM-DATE-TIME.*", line)][0][1])
-    print(start_time)
+    segments_independant = bool([line for line in lines if re.match(".*INDEPENDENT.*", line)])
 
-    segments_independant = bool([line for line in lines if re.match(".*INDEPENDANT.*", line)])
-    print(segments_independant)
+    stream_start: time = start_time.time()
+    date = start_time.date()
+    clip_start = time.fromisoformat("14:53:14")
+    clip_start_dt = datetime(date.year, date.month, date.day, clip_start.hour, clip_start.minute, clip_start.second)
+    start_offset = clip_start_dt - start_time
+    print(start_offset)
+
+    if segments_independant and unique_durations == 1:
+        start_segment = int(start_offset.seconds // actual_durations[0])
+        print(start_segment)    
+
+    clip_end = time.fromisoformat("14:54:27")
+    clip_end_dt = datetime(date.year, date.month, date.day, clip_end.hour, clip_end.minute, clip_end.second)
+    end_offset = clip_end_dt - start_time
+
+    if segments_independant and unique_durations == 1:
+        end_segment = math.ceil(end_offset.seconds/actual_durations[0])
+        print(end_segment)
 
 
 if __name__ == "__main__":
